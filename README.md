493d29d9417b0af708bc775849395fce019c74e2
mobren11 <mobren11@gmail.com>
2025-04-19 17:18:21 -0500

    inital commit (complete)
    write and document script to act as wrapper to https://www.toptal.com/developers/javascript-minifier/documentation/curl . See `README.md` for more info

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # js minfier shell
- Wrapper for https://www.toptal.com/developers/javascript-minifier
+ # Purpose
+  Often, when I am developing JavaScript projects, I find myself using https://www.toptal.com/developers/javascript-minifier to minify my code. Because minified js code is basically impossible to edit, however, I keep a "source code" copy nearby. This usually ends up in the below structure.
+
+  ```
+    ./Code/
+    ├── exe
+    │   ├── URI-manager.js
+    │   ├── format.js
+    │   ├── main.js
+    │   ├── provider.js
+    │   ├── settings.js
+    │   └── treeblocks.js
+    ├── lib
+    │   ├── lzma-min.js
+    │   └── pako-min.js
+    └── src
+        ├── URI-manager.js
+        ├── format.js
+        ├── main.js
+        ├── provider.js
+        ├── settings.js
+        └── treeblocks.js
+  ```
+  This shellscript takes in a pathname as a parameter and searches for occurrences of `/[cC]ode/src/.*\.js` and minifies them to `/[cC]ode/exe/.*\.js`, curtesy of the [Toptal JS-Minifier API](https://www.toptal.com/developers/javascript-minifier/documentation/curl)
+
+ # Usage
+  Simply provide a path as the parameter during execution
+  ```
+  minify.sh <pathname>
+  ```
+  ```
+  minify.sh ./../MyRepo
+  ```
---
+++ minify.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# minifies all .js files downwind from the specified path if they are located in a /[cC]ode/src/ and writes them to /[cCode]/exe/
+
+root=$1
+
+files=$(find $root -path "*/[cC]ode/src/*.js")
+
+now=$(date)
+
+for file in $files; do
+    # gather relevant info
+    source_path=$(realpath "$file")
+    source_file=$(basename "$source_path")
+    source_dir=$(dirname "$source_path")
+    output_dir=$(realpath "${source_dir}/../exe")
+    output_file="${output_dir}/${source_file}";
+
+    # do the curl POST to the API endpoint
+    source_content=$(cat "$source_path");
+    header=$(echo -e "// MINIFIED VERSION OF THE FILE OF THE SAME NAME IN THE \`src\` FOLDER\n// MINIFIED WITH https://www.toptal.com/developers/javascript-minifier\n// MINIFIED AT ${now}")
+    response=$(curl -X POST -s --data-urlencode "input=$source_content" https://www.toptal.com/developers/javascript-minifier/api/raw)
+
+    # write result
+    mkdir "$output_dir" 2>/dev/null # make the output `exe` dir if it doesn't already exist
+    touch "$output_file" # touch the output file to make sure it exists
+    printf '%s\n\n%s' "$header" "$response" > "$output_file" # redirect the data to the output file
+
+
+    # print to console
+    echo "Minified \`${source_path}\` ---> \`${output_file}\`"
+    sleep 3; # wait three seconds as to not overwhelm the API-- will return `error code: 1015` if rate limit is exceeded
+done
